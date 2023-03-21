ee48389fa3fff9c8c3b99406e23902fcabf3c896
Jatin Choudhary <jatinranjit@gmail.com>
2023-03-21 20:43:34 +0530

    chatgpt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 interface Message {
-  clientId: number;
+  sender: string;
   message: string;
-  clientmsg: string;
 }
 
 function App(): JSX.Element {
-  const [clientId, setClientId] = useState<number>(Math.floor(new Date().getTime() / 1000));
-
   const [messages, setMessages] = useState<Message[]>([]);
   const [textValue, setTextValue] = useState<string>("");
-  const [msgTab, setmsgTab] = useState<string[]>([]);
-  const [websocket, setWebsocket] = useState<WebSocket>();
 
-  useEffect(() => {
-    const url = `ws://localhost:8000/ws/${clientId}`;
-    const ws = new WebSocket(url);
-    ws.onopen = () => {
-      ws.send("Connect");
-    };
-    ws.onmessage = (event) => {
-      const message: Message = JSON.parse(event.data);
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
-    setWebsocket(ws);
-    return () => ws.close();
-  }, [clientId]);
+  const sendMessage = async () => {
+    if (textValue.trim() === "") return;
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer 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`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: `The following is a conversation between a user and an AI. The user says "${textValue}" and the AI responds.` }],
 
-  const sendMessage = () => {
-    if (!websocket) return;
-    setmsgTab([...msgTab, textValue]);
-    websocket.send(textValue);
+        temperature: 0.7,
+      }),
+    });
+
+    if (!response.ok) {
+      console.log("Error: ChatGPT API call failed.");
+      return;
+    }
+
+    const data = await response.json();
+    const aiResponse = data.choices[0].text.trim();
+
+    setMessages((prevMessages) => [...prevMessages, { sender: "user", message: textValue }]);
+    setMessages((prevMessages) => [...prevMessages, { sender: "ai", message: aiResponse }]);
+
     setTextValue("");
   };
-
   return (
     <div className="bg-cyan-100 h-screen flex justify-center">
       <div className="bg-cyan-200 w-1/3 h-98 overflow-y-scroll my-4 rounded-xl flex flex-col">
         <div className="flex justify-center text-center items-center py-2 bg-cyan-500 h-12 rounded-t-xl">
-          <h2>Your client id: {clientId} </h2>
+          <h2>Chat with AI</h2>
         </div>
         <div className="h-98 overflow-y-scroll ">
           {messages.map((message, index) => {
-            const isMyMessage = message.clientId === clientId;
+            const isMyMessage = message.sender === "user";
             return (
               <div key={index} className="w-full h-30 rounded-xl flex flex-col">
-                {message.clientmsg != "" && (
+                {message.message != "" && (
                   <div className="w-full flex items-center">
                     <div className="w-9 h-9 mx-2 rounded-full bg-white flex justify-center items-center">
                       <img src="/user.png" alt="" className="w-6 h-6 rounded-full" />
                     </div>
                     <div className="bg-cyan-300 max-w-[75%] w-fit mx-2 rounded-xl my-1 flex items-center">
-                      <p className="px-2 py-1 pb-2">{message.clientmsg}</p>
+                      <p className="px-2 py-1 pb-2">{message.message}</p>
                     </div>
                   </div>
                 )}
@@ -84,3 +87,4 @@ function App(): JSX.Element {
 }
 
 export default App;
+//key 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
